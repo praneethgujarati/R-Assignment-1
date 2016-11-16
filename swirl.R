1973a0d18b1c6035c92b40a9c4381e17b7148188
praneethgujarati <praneeth705@gmail.com>
2016-11-16 19:42:13 +0530

    Add files via upload

---
+++ swirl.R
@@ -0,0 +1,39 @@
+installed.packages("swirl")
+install.packages("swirl")
+swirl()
+search()
+install.packages("swirl")
+search()
+library("swirl")
+swirl()
+praneeth
+2
+1
+1
+2
+getwd()
+ls()
+x<-9
+ls()
+dir()
+?list.files
+args(list.files)
+old.dir<-getwd()
+dir.create("testdir")
+setwd("testdir")
+file.create("mytest.R")
+ls()
+list.files("mytest.R")
+list.files()
+file.exists("mytest.R")
+file.info("mytest.R")
+file.rename("mytest.R", "mytest2.R")
+file.copy("mytest2.R", "mytest3.R")
+file.path("mytest3.R")
+file.path("folder1","folder2")
+?dir.create
+dir.create(file.path("testdir2", "testdir3"), recursive = TRUE)
+setwd(old.dir)
+unlink("testdir",recursive = TRUE)
+2
+0
